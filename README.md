acda339c927cc3487f5df89e28a7887769820fad
Ulrike Fischer <fischer@troubleshooting-tex.de>
2021-05-02 20:42:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# mparhack package for LaTeX
+
+Version: 2005-04-17 v1.4
+
+This package implements a workaround for the LATEX bug that marginpars
+sometimes appear on the wrong margin.
+
+
+
+## Copyright (C)
+* 2005             Tom Sgouros, Stefan Ulrich
+* 2021             LaTeX Project Team
+
+## License
+LATEX Project Public License, version 1.3c or later.
---
+++ build.lua
@@ -0,0 +1,51 @@
+#!/usr/bin/env texlua
+
+
+module = "mparhack"
+
+installfiles = {
+'*.sty',
+}
+
+sourcefiles = {"*.dtx"}
+
+checkruns=2
+
+textfiles = {"README.md"}
+unpackfiles = {'mparhack.dtx'}
+
+
+function update_tag(file,content,tagname,tagdate)
+
+tagfiles = {"*.dtx", "*.md"}
+
+local tagpattern="(%d%d%d%d[-/]%d%d[-/]%d%d) v(%d+[.])(%d+)"
+local oldv,newv
+if tagname == 'auto' then
+  local i,j,olddate,a,b
+  i,j,olddate,a,b= string.find(content, tagpattern)
+  if i == nil then
+    print('OLD TAG NOT FOUND')
+    return content
+  else
+    print ('FOUND: ' .. olddate .. ' v' .. a .. b )
+    oldv = olddate .. ' v' .. a .. b
+    newv = tagdate .. ' v'  .. a .. math.floor(b + 1)
+    print('USING OLD TAG: ' .. oldv)
+    print('USING NEW TAG: ' .. newv)
+    local oldpattern = string.gsub(oldv,"[-/]", "[-/]")
+    content=string.gsub(content,"{Version}{" .. oldpattern,'##OLDV##')
+    content=string.gsub(content,oldpattern,newv)
+    content=string.gsub(content,'##OLDV##',"{Version}{" .. oldv)
+    content = string.gsub(content,
+        '%% \\end{History}',
+	'%%   \\begin{Version}{' .. newv .. '}\n%%   \\item Updated\n%%   \\end{Version}\n%% \\end{History}')
+    return content
+  end
+else
+  error("only automatic tagging supported")
+end
+
+end
+
+
